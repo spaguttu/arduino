3e53751fd5d91548f46df064b91a562b92264b54
Pietro <44373440+spaguttu@users.noreply.github.com>
2018-10-24 19:00:32 +0200

    Update controllaledconseriale.ino

--- controllaledconseriale.ino
+++ controllaledconseriale.ino
@@ -1,13 +1,14 @@
-void setup() { 
+void setup()
+{ 
   Serial.begin(9600);
   pinMode(13, OUTPUT);
 }
-void loop() {
-
+void loop()
+{
  int legge= Serial.parseInt();
  if (legge > 0) {
   Serial.print(legge); 
     analogWrite(13,legge); 
     Serial.flush();
-    
-         }     }
+     }  
+}
