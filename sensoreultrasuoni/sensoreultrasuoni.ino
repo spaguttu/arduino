f4829a1b43790adefee85a2a4e77b977fc9b80ec
Pietro <44373440+spaguttu@users.noreply.github.com>
2018-12-14 13:24:17 +0100

    Update sensoreultrasuoni.ino

--- sensoreultrasuoni/sensoreultrasuoni.ino
+++ sensoreultrasuoni/sensoreultrasuoni.ino
@@ -4,9 +4,9 @@ int echo=8;// pin echo del sensore
 int distanza;//convertito dal valore che mi passa il sensore
 int durata;//
 void setup() {
-pinMode(bazz,OUTPUT); // uso il pin del buzzer come output
-pinMode(trig, OUTPUT);//trig è un output
-pinMode(echo,INPUT);//echo è l'input
+pinMode(bazz,OUTPUT);
+pinMode(trig, OUTPUT);
+pinMode(echo,INPUT);
 digitalWrite(5,HIGH);
 Serial.begin(9600);
  
@@ -17,9 +17,9 @@ delayMicroseconds(2);//spetta un pochetto
 
 digitalWrite(trig,HIGH);
 delayMicroseconds(10);
-digitalWrite(trig,LOW);// fa andare il trig per 10 micro secondi
+digitalWrite(trig,LOW);//passa corrente al pin trig 10 micro secondi
 
-durata = pulseIn(echo,HIGH); //vedi sopra,da capire
+durata = pulseIn(echo,HIGH); //vedi reference da arduino,non precisissimo
 
 distanza= durata*0.034/2;//formuletta per convertire il pulse in distanza,espressa in cm
 
